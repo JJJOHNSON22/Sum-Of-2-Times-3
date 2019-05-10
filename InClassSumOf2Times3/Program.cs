ce5737151fda76bd5e72f3a66649b9e91f0a0767
Jerome James Johnson <46817141+JJJOHNSON22@users.noreply.github.com>
2019-05-10 13:36:19 -0400

    Add project files.

---
+++ InClassSumOf2Times3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassSumOf2Times3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number 1 is: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number 2 is: ");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+
+            //if the first number is equal to the second number than the sum of those two numbers will be multiplied by three
+            if (a1 == b1)
+            {
+                Console.WriteLine($"({a1} + {b1}) * 3 = {(a1 + b1) * 3}");
+            }
+            else
+            {
+                Console.WriteLine($"{a1} + {b1} = {a1 + b1}");
+            }
+        }
+    }
+}
